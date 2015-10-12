4fc38ade505b5a795c993d853e60217018c02a45
Watersdr <watersdr@rose-hulman.edu>
2015-10-11 21:42:09 -0400

    cleaned up some stuff on index page with date selecting

---
+++ static/scripts/trconf.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	// Setup - add a text input to each header cell
+	$('#datatable thead th').each(function() {
+		var title = $('#datatable thead th').eq($(this).index()).text();
+		$(this).append('<input type=\'text\' placeholder=\'Search ' + title + '\'/>');
+	});
+
+	// DataTable
+	var datatable = $('#datatable').DataTable();
+	$('#datatable_filter').css('display: none');
+
+	// Apply the search
+	datatable.columns().eq(0).each(function(colIdx) {
+		$('input', datatable.column(colIdx).header()).on('keyup change', function() {
+			datatable
+			.column(colIdx)
+			.search(this.value)
+			.draw();
+		});
+
+		$('input', datatable.column(colIdx).header()).on('click', function(e) {
+			e.stopPropagation();
+		});
+	});
+});
