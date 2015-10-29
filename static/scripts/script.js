b81eb8bfab7b9970e7c87ddd6f76ab3a6b92c401
Watersdr <watersdr@rose-hulman.edu>
2015-10-28 22:40:47 -0400

    charts woooooo

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
+
 	$("#foot01").innerHTML =
 	"<p>&copy; " + new Date().getFullYear() + " ProfitLossPortal</p>";
 
-	var ctx = document.getElementById("myChart").getContext("2d");
-	var myNewChart = new Chart(ctx).PolarArea(data);
-	
 	$('.date_radio').on('click', function(e) {
 		if ($('#selected_date:checked').val()) {
 			$('#date_selectors').show();
--- static/scripts/trconf.js
+++ static/scripts/trconf.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	// $('#myChart').hide();
+    var canvas_html = '<canvas id="myChart" class="col-sm-6"></canvas>';
+    $('#hideChartButton').hide();
+
 	// Setup - add a text input to each header cell
 	$('#datatable thead th').each(function() {
 		var title = $('#datatable thead th').eq($(this).index()).text();
@@ -7,7 +11,7 @@ $(document).ready(function () {
 
 	// DataTable
 	var datatable = $('#datatable').DataTable();
-	$('#datatable_filter').css('display: none');
+	$('#datatable_filter').hide();
 
 	// Apply the search
 	datatable.columns().eq(0).each(function(colIdx) {
@@ -22,4 +26,48 @@ $(document).ready(function () {
 			e.stopPropagation();
 		});
 	});
+
+	$('#hideChartButton').on('click', function () {
+		$('#chart_container').html("");
+		$('#hideChartButton').hide();
+	});
+
+	$('#getChartButton').on('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_get_transactions', {
+        account: $('input[name="account_id"]').val(),
+        stock_sym: $('input[name="stock_sym"]').val()
+      }, function(data) {
+		$('#hideChartButton').show();
+        var data_list = data.values;
+        var label_list = data.labels;
+        // alert(data_list + label_list);
+        var graph_data = {
+    		labels: label_list,
+    		datasets: [
+        		{
+            		label: "My First dataset",
+            		fillColor: "rgba(220,220,220,0.2)",
+            		strokeColor: "rgba(220,220,220,1)",
+            		pointColor: "rgba(220,220,220,1)",
+            		pointStrokeColor: "#fff",
+            		pointHighlightFill: "#fff",
+            		pointHighlightStroke: "rgba(220,220,220,1)",
+            		data: data_list
+        		}
+        	]
+		};
+		var options = {
+			scaleShowGridLines : true
+		};
+
+		$('#chart_container').html(canvas_html);
+
+        var ctx = $("#myChart").get(0).getContext("2d");
+		var myNewChart = new Chart(ctx).Line(graph_data, options);
+		// $('#myChart').attr('width', 400)
+		// $('#myChart').attr('height', 400)
+		// $('#myChart').show();
+      });
+      return false;
+    });
 });
