c9710cb5d0bb06a452f6f8e7379404303cdf5eba
Shane S Bruggeman <shanebruggeman@rose-hulman.edu>
2015-11-04 01:32:24 -0500

    Merging in changes to db insert so that tests can reflect that for parser

--- parser/test/test_parsing.py
+++ parser/test/test_parsing.py
@@ -1,19 +1,27 @@
-import sys
-import re
 import unittest
-import parser
-from parser import *
+import parse
 
-testdata_file = "test_data.txt"
-test_maketake_file = "test_maketake.txt"
+testdata_file = "test/example_parse_data.txt"
+test_maketake_file = "test/example_maketake.txt"
+
+transaction_required_values = [
+	'TransactTime',
+	'PutOrCall',
+	'settle',
+	'entry',
+	'trade',
+	'ticket_number',
+	'buy_sell',
+	'commission'
+]
 
 class TestParser(unittest.TestCase):
 
-	def test_true(self):
-		self.assertTrue(True)
+	def test_parsed_data_contains_all_necessary_attributes(self):
+		parsed_result = parse.parse_transactions(testdata_file, test_maketake_file, "Bat")
 
-	def test_false(self):
-		self.assertTrue(True)
+	def test_make_take_levels_are_appropriately_nested(self):
+		pass
 
 if __name__ == '__main__':
     unittest.main()
