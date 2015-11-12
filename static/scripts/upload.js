1c2a32a949d6311b54356c4c6f8702a09021ea8d
Watersdr <watersdr@rose-hulman.edu>
2015-11-12 13:53:23 -0500

    made the upload page look a little bit nicer. added a 404 page

--- static/scripts/upload.js
+++ static/scripts/upload.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
-// Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-var previewNode = $("#template");
+
+// Get the template HTML and remove it from the document template HTML and remove it from the doument
+var previewNode = document.querySelector("#template");
 previewNode.id = "";
 var previewTemplate = previewNode.parentNode.innerHTML;
 previewNode.parentNode.removeChild(previewNode);
 
 var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-  url: "/target-url", // Set the url
+  url: "/upload", // Set the url
   thumbnailWidth: 80,
   thumbnailHeight: 80,
   parallelUploads: 20,
   previewTemplate: previewTemplate,
   autoQueue: false, // Make sure the files aren't queued until manually added
   previewsContainer: "#previews", // Define the container to display the previews
-  clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+  clickable: "#submit-files" // Define the element that should be used as click trigger to select files.
 });
 
 myDropzone.on("addedfile", function(file) {
@@ -21,30 +22,14 @@ myDropzone.on("addedfile", function(file) {
   file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
 });
 
-// Update the total progress bar
-myDropzone.on("totaluploadprogress", function(progress) {
-  $("#total-progress .progress-bar").style.width = progress + "%";
+myDropzone.on("success", function(file) {
+  // Hookup the start button
+  file.previewElement.querySelector("#progress-message").innerHTML = "Success";
 });
 
 myDropzone.on("sending", function(file) {
-  // Show the total progress bar when upload starts
-  $("#total-progress").style.opacity = "1";
   // And disable the start button
   file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
 });
 
-// Hide the total progress bar when nothing's uploading anymore
-myDropzone.on("queuecomplete", function(progress) {
-  $("#total-progress").style.opacity = "0";
-});
-
-// Setup the buttons for all transfers
-// The "add files" button doesn't need to be setup because the config
-// `clickable` has already been specified.
-$("#actions .start").onclick = function() {
-  myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-};
-$("#actions .cancel").onclick = function() {
-  myDropzone.removeAllFiles(true);
-};
 });
