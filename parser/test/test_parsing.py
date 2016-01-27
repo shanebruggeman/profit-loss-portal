2f3a0413da465f6474a5531b569731747dc7dc9b
Joe McNelly <csse@profitlossportal.csse.rose-hulman.edu>
2016-01-27 10:05:36 -0500

    slight updates to tests

--- parser/test/test_parsing.py
+++ parser/test/test_parsing.py
@@ -1,6 +1,8 @@
 import unittest
 import sys
-sys.path.append('../')
+import os
+#sys.path.append('../')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import parse
 
 
