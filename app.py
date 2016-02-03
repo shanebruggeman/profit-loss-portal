bad68fff61fab91fe302e68a1e01dc8074a4ef8c
Zach Hull <hullzr@rose-hulman.edu>
2016-02-03 00:21:22 -0500

    bolding is working...feelsgoodman.jpg

--- app.py
+++ app.py
@@ -109,19 +109,19 @@ def newplreport(account, date):
 			# 		itemTotal += SEC_fee + broker_fee; ##Need to add exchange fee
 			# grand_total +=itemTotal		
 			# stock_dict[initSymb] = itemTotal
+	# create dictionary of transactions in a closing position
 	for symbol in stock_dict:
 		for option in stock_dict[symbol]:
 			current_quantity = 0
 			bold_dict[option] = []
 			trans_to_bold = []
 			for trans in stock_dict[symbol][option]:
-				
-				print(trans.entry)
-				print(trans.units)
-				current_quantity = current_quantity + trans.units
-				print(current_quantity)
-				if current_quantity == 0:
-					del trans_to_bold[:]
+				if trans.isPosition == False:
+					current_quantity = current_quantity + trans.units
+					if current_quantity == 0:
+						del trans_to_bold[:]
+					else:
+						trans_to_bold.append(trans.transaction_id)
 				else:
 					trans_to_bold.append(trans.transaction_id)
 
@@ -131,8 +131,6 @@ def newplreport(account, date):
 				# 
 				# closing_position = StockPosition(last_trans.sec_sym, last_trans.settle ,last_trans.account_id)
 				# closing_position.all_transactions.append(stock_dict[symbol][option])
-	print("bold_dict goes here")
-	print(bold_dict)
 
 	# return render_template('plreport.html', transList = transactionList, totalProfit=grand_total, numTrades= num_trades, list=stock_names, dict=stock_dict, period = time_period)
 	return render_template('newplreport.html', stockdict=stock_dict, period=time_period, bolddict=bold_dict)
